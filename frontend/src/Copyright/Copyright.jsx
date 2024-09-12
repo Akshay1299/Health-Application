066f645f5adff5c65440c8bb85587f8e4a88df50
vanshdeepsingh549 <singh24907@gmail.com>
2024-09-12 22:48:11 +0530

    Making Changes

---
+++ frontend/src/Copyright/Copyright.jsx
@@ -0,0 +1,15 @@
+import "./Copyright.css";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Vanshdeep Singh. All rights reserved.</p>
+
+      <p>
+        Website by <a href="https://github.com/vanshdeepsingh549">Vanshdeep Singh</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
